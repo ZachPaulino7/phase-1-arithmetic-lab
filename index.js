2a5f06afde231d78958f39711f1d34569f261752
Zachpaulino7 <Zachp7@live.com>
2021-08-11 01:01:59 -0500

    commit tree 6c4bb8df1a50bb0ac4af15e5f22181f1d857c0be

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 10;
+const num4 =6;
 
-// Write your code here
+let multiply = num1 *num2;
+
+let random = Math.ceil(Math.random() * 20);
+    
+let mod = num3 % num4;
+
+let max = Math.max(10,11,13,15,18,20);
